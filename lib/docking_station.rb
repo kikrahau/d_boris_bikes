ccc9e4f55f2f78db269c59c97e79d32b05602f33
kikrahau <vincent.koch@hotmail.com>
2014-08-18 15:32:20 +0100

    create class docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,2 +1,27 @@
 class Docking_Station
+	IS_BROKEN = ->(bike){bike.broken?}
+
+	def initialize(bikes=[])
+		@bikes = bikes
+	end
+
+	def bikes
+		@bikes
+	end
+
+	def broken_bikes
+		@bikes.select &IS_BROKEN 
+	end
+	
+	def working_bikes
+		@bikes.reject &IS_BROKEN
+	end
+	
+	def dock(bike)
+		@bikes << bike
+	end
+
+	def release_bike
+		@bikes.pop
+	end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,41 @@
 require 'docking_station'
 
 describe Docking_Station do
-	it "should.." do
+	let(:station) { Docking_Station.new }
+	let(:bike) { double :bike, broken?: false }
+	let(:broken_bike) { double :broken_bike, broken?: true}
+
+	let(:station_with_bikes) { Docking_Station.new([bike]) }
+	let(:station_with_broken_bikes) {Docking_Station.new([broken_bike,bike])}
+
+	it "should return an empty array of bikes by default" do
+		expect(station.bikes).to eq []
 	end
 
+	it "should be able to be created containing bikes" do
+		
+		expect(station_with_bikes.bikes).to eq [bike]
+	end
+
+	it "should be able to accept a bike" do
+		station.dock(bike)
+		expect(station.bikes).to eq [bike]
+	end
+
+	it "should be able to release a bike" do
+		station_with_bikes.release_bike
+		expect(station_with_bikes.bikes).to eq []
+	end
+
+	it "should know what broken_bikes it has" do
+		expect(station_with_broken_bikes.broken_bikes).to eq [broken_bike]
+	end	
+
+	it "should know what working bikes it has" do
+		expect(station_with_broken_bikes.working_bikes).to eq [bike]
+	end
+
+	it "should be able to release broken_bikes" do
+		expect(station_with_broken_bikes.release_bike).to eq [broken_bike]
+	end
 end
