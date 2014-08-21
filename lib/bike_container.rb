26acf3a4f84e783b28b9d2cbe44ba90678605b6a
benjamintillett <benjamin.tillett@gmail.com>
2014-08-21 13:07:39 +0100

    removing redundant methods

--- lib/bike_container.rb
+++ lib/bike_container.rb
@@ -1,7 +1,7 @@
 module BikeContainer
 
-	IS_BROKEN = ->(bike){bike.broken?}
-	IS_WORKING = ->(bike){!bike.broken?}
+	BROKEN = ->(bike){bike.broken?}
+	WORKING = ->(bike){!bike.broken?}
 	ALL = -> (bike) { true }
 
 	DEFAULT_CAPACITY = 20
@@ -29,14 +29,6 @@ module BikeContainer
 	def capacity=(value)
 		@capacity = value
 	end
-
-	def broken_bikes
-		bikes_of_type(IS_BROKEN)
-	end
-	
-	def working_bikes
-		bikes_of_type(IS_WORKING)
-	end
 	
 	def dock(bike)
 		@bikes << bike
@@ -46,27 +38,20 @@ module BikeContainer
 		@bikes.pop
 	end
 
-	def release_broken_bikes
-		broken = broken_bikes
-		broken_bikes.pop
-		broken
-	end
-
 	def full?
 		@bikes.length == capacity
 	end
 
 	def get_bikes_from(container, bike_type=ALL)
-		# all, broken, working
 		@bikes += container.bikes_of_type(bike_type)
 		container.bikes = container.bikes.reject &bike_type
 	end
 
 	def get_broken_bikes_from(container)
-		get_bikes_from(container, IS_BROKEN)
+		get_bikes_from(container, BROKEN)
 	end
 
 	def get_working_bikes_from(container)
-		get_bikes_from(container, IS_WORKING)
+		get_bikes_from(container, WORKING)
 	end
 end
